920c51ecc4d8301a120b590ad5e9a709c82aa4e9
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-03-14 16:41:31 -0300

    Add testes

--- src/routes/task.js
+++ src/routes/task.js
@@ -102,11 +102,12 @@ router.delete('/tasks/:id', auth, async (req, res) => {
     });
 
     if (!task) {
-      res.status(404).send('Task not found!');
+      res.status(404).send();
     }
-    res.status(200).send(task);
+
+    res.send(task);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).send();
   }
 });
 
